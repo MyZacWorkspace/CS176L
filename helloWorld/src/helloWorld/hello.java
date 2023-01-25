66de02d4475c57b36ae9f91e4bc390c82bf70671
MyZacWorkspace <114018512+MyZacWorkspace@users.noreply.github.com>
2023-01-24 19:50:55 -0500

    my first java program

---
+++ helloWorld/src/helloWorld/hello.java
@@ -0,0 +1,10 @@
+package helloWorld;
+//First Assignment to be uploaded to CS176L Repo
+//Zaccery Tarver 1.24.2023
+public class hello 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
+}
