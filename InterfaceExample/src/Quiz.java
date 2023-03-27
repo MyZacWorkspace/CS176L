368e711d73a7228442adafeb474abe2872b884c2
zacMonRepo <114018512+MyZacWorkspace@users.noreply.github.com>
2023-03-27 15:14:55 -0400

    InterfaceExample

---
+++ InterfaceExample/src/Quiz.java
@@ -0,0 +1,24 @@
+//Quiz Class 3.27
+public class Quiz implements Measurable
+{
+	private double score;
+	
+	public Quiz(double score)
+	{
+		if(score < 0)
+		{
+			this.score = 0;
+		}
+		else if(score > 100)
+		{
+			this.score = 100;
+		}
+		
+		this.score = score;
+	}
+	
+	public double getMeasure()
+	{
+		return score;
+	}
+}
